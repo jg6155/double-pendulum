d7279d3f7cbf6206b1c072a380473e21dcb2c3dd
DanniTan96 <det278@nyu.edu>
2023-12-07 19:57:54 -0500

    Update LeapFrog.py (RuntimeWarning)

--- LeapFrog.py
+++ LeapFrog.py
@@ -1,128 +1,122 @@
 import numpy as np 
 import matplotlib.pyplot as plt
-from animator import PendulumAnimator
+# from animator import PendulumAnimator
 
-def W_Dot(W, Theta, t):
-	"""
-	Calculating each successive value of Angular Acceleration (W_Dot) 
-	using the previous value of W and Theta
-	"""
-	g = 9.8
-	l = 0.4
-	w1 = W[0]
-	w2 = W[1] 
-	theta1 = Theta[0]
-	theta2 = Theta[1]
-	w1_dot = (w1**2*np.sin(2*theta1 - 2*theta2) + 2*w2**2*np.sin(theta1 - theta2) \
-			+ (g/l)*(np.sin(theta1 - 2*theta2) + 3*np.sin(theta1))) / (np.cos(2*theta1 - 2*theta2) - 3)
-	w2_dot = (4*w1**2*np.sin(theta1 - theta2) + w2**2*np.sin(2*theta1 - 2*theta2) \
-			+ 2*(g/l)*(np.sin(2*theta1 - theta2) - np.sin(theta2))) / (3 - np.cos(2*theta1 - 2*theta2))
-	return np.array([w1_dot, w2_dot],float)
+m = 1.0
+g = 9.8
+L = 0.4
+ti = 0.0
+tf = 5
+N = 100
+h = (tf - ti)/N
+tpoints = np.arange(ti, tf, h)
+# w1_dot = (w1**2*np.sin(2*theta1 - 2*theta2) + 2*w2**2*np.sin(theta1 - theta2) + (g/l)*(np.sin(theta1 - 2*theta2) + 3*np.sin(theta1))) / (np.cos(2*theta1 - 2*theta2) - 3)
+# return w1_dot
 
-def Theta_Dot(W, Theta, t):
-	"""
-	Calculating each successive value of Angular Velocity (Theta_Dot, equivalently W)
-	using the previous value of W
-	"""
-	g = 9.8
-	l = 0.4
-	w1 = W[0]
-	w2 = W[1] 
-	theta1 = Theta[0]
-	theta2 = Theta[1]
-	theta1_dot = w1
-	theta2_dot = w2
-	return np.array([theta1_dot, theta2_dot], float)
+# w2_dot = (4*w1**2*np.sin(theta1 - theta2) + w2**2*np.sin(2*theta1 - 2*theta2) + 2*(g/l)*(np.sin(2*theta1 - theta2) - np.sin(theta2))) / (3 - np.cos(2*theta1 - 2*theta2))
+# return w2_dot
+
+
+def w1_dot(w1, w2, theta1, theta2):
+	w1_term = w1**2*(np.sin(2*theta1 - 2*theta2))
+	w2_term = 2*w2**2*np.sin(theta1 - theta2)
+	numerator = w1_term*(-1) - w2_term - (g/L)*(np.sin(theta1 - 2*theta2) + 3*np.sin(theta1))
+	denom = 3 - np.cos(2*theta1 - 2*theta2)
+	return numerator/denom
+
+
+def w2_dot(w1, w2, theta1, theta2):
+	w1_term = 4*w1**2*(np.sin(theta1 - theta2))
+	w2_term = w2**2*(np.sin(2*theta1 - 2*theta2))
+	numerator = w1_term + w2_term + (2*g/L)*(np.sin(2*theta1 - theta2) - np.sin(theta2))
+	denom = 3 - np.cos(2*theta1 - 2*theta2)
+	return numerator/denom
 
 def LeapFrog_Int():
-	"""Constants of Integration and Time Interval"""
-	g = 9.8
-	l = 0.4
-	ti = 0.0
-	tf = 10.0
-	N = 1000
-	h = (tf - ti)/N
-
 	"""Initial Conditions"""
-	theta1_i = np.pi/2
-	theta2_i = np.pi/2
-	w1_i = 0.0
-	w2_i = 0.0 
-	Theta_i = np.array([theta1_i, theta2_i], float)
-	W_i = np.array([w1_i, w2_i], float)
+	theta1_init = np.pi/2
+	theta2_init = np.pi/2
+	w1_init = 0.0
+	w2_init = 0.0 
 
-	"""Lists to store desired values. The Thetas will be used to animate, and both the Thetas
-	and the Ws will be used to test the energy conservation
-	"""
-	tpoints = np.arange(ti, tf, h)
-	theta1_points = []
-	theta2_points = []
-	w1_points = []
-	w2_points = []
+	"""Arrays to store my values"""
+	w1 = np.zeros(N)
+	w2 = np.zeros(N)
+	theta1 = np.zeros(N)
+	theta2 = np.zeros(N)
+	w1_halves = np.zeros(N)
+	w2_halves = np.zeros(N)
+	theta1_halves = np.zeros(N)
+	theta2_halves = np.zeros(N)
 
-	"""Calculating the first HALF step of W & Theta using Euler's method"""
-	W_first_half = W_i + 0.5*h*W_Dot(W_i, Theta_i, 0)
-	Theta_first_half = Theta_i + 0.5*h*Theta_Dot(W_i, Theta_i, 0) 
+	"""Initial Half Steps with Euler's Method"""
+	w1_FirstHalf = w1_init + 0.5*h*w1_dot(w1_init, w2_init, theta1_init, theta2_init)
+	w2_FirstHalf = w2_init + 0.5*h*w2_dot(w1_init, w2_init, theta1_init, theta2_init)
 
-	"""Calculating the first FULL step of W & Theta using Euler's method"""
-	W_first_full = W_i + h*W_Dot(W_first_half, Theta_first_half, 0.5*h)
-	Theta_first_full = Theta_i + 0.5*h*Theta_Dot(W_first_half, Theta_first_half, 0.5*h)
+	theta1_FirstHalf = theta1_init ##Initial velocity is zero
+	theta2_FirstHalf = theta2_init ##Initial velocity is zero
 
-	"""Leapfrog Integration Loop"""
-	for t in tpoints:
-		W_first_full += h*W_Dot(W_first_half, Theta_first_half, (t+0.5*h)) 
-		Theta_first_full += h*Theta_Dot(W_first_half, Theta_first_half, (t+0.5*h))
+	"""Initial Full Steps with Euler's Method"""	
+	w1_FirstFull = w1_init + h*w1_dot(w1_FirstHalf, w2_FirstHalf, theta1_FirstHalf, theta2_FirstHalf)
+	w2_FirstFull = w2_init + h*w2_dot(w1_FirstHalf, w2_FirstHalf, theta1_FirstHalf, theta2_FirstHalf)
 
-		W_first_half += h*W_Dot(W_first_full, Theta_first_full, (t+h))
-		Theta_first_half += h*Theta_Dot(W_first_full, Theta_first_full, (t+h))
+	theta1_FirstFull = theta1_init + h*w1_FirstHalf
+	theta2_FirstFull = theta2_init + h*w2_FirstHalf
 
-		theta1_points.append(Theta_first_full[0])
-		theta2_points.append(Theta_first_full[1])
-		w1_points.append(W_first_full[0])
-		w2_points.append(W_first_full[1])
+	w1[0] = w1_init
+	w1[1] = w1_FirstFull
+	w2[0] = w2_init
+	w2[1] = w2_FirstFull
+	theta1[0] = theta1_init
+	theta1[1] = theta1_FirstFull
+	theta2[0] = theta2_init
+	theta2[1] = theta2_FirstFull
+
+	w1_halves[0] = w1_FirstHalf
+	w2_halves[0] = w2_FirstHalf
+	theta1_halves[0] = theta1_FirstHalf
+	theta2_halves[0] = theta2_FirstHalf
+
+	"""Integration Loop"""
+	for i in range(N-2):
+		w1_halves[i+1] = w1_halves[i] + h*w1_dot(w1[i+1], w2[i+1], theta1[i+1], theta2[i+1])
+		w2_halves[i+1] = w2_halves[i] + h*w2_dot(w1[i+1], w2[i+1], theta1[i+1], theta2[i+1])
+		theta1_halves[i+1] = theta1_halves[i] + h*w1[i+1]
+		theta2_halves[i+1] = theta2_halves[i] + h*w2[i+1]
+
+		w1[i+2] = w1[i+1] + h*w1_dot(w1_halves[i+1], w2_halves[i+1], theta1_halves[i+1], theta2_halves[i+1])
+		w2[i+2] = w2[i+1] + h*w2_dot(w1_halves[i+1], w2_halves[i+1], theta1_halves[i+1], theta2_halves[i+1])
+		theta1[i+2] = theta1[i+1] + h*w1_halves[i+1]
+		theta2[i+2] = theta2[i+1] + h*w2_halves[i+1]
 
 	"""Plotting the Test Data"""
-	# plt.plot(tpoints, theta1_points, 'b-', label = r'$\theta_1$')
-	# plt.plot(tpoints, theta2_points, 'r-', label = r'$\theta_2$')
-	# plt.plot(tpoints, w1_points, 'k-', label = r'$\omega_1$')
-	# plt.plot(tpoints, w2_points, 'g-', label = r'$\omega_2$')
-	# plt.xlabel('Time (s)')
-	# plt.ylabel('Time-Dependent Variable')
-	# plt.legend(loc = 'best')
-	# plt.show()
+	plt.plot(tpoints, theta1, 'b-', label = r'$\theta_1$')
+	plt.plot(tpoints, theta2, 'r-', label = r'$\theta_2$')
+	plt.plot(tpoints, w1, 'k-', label = r'$\omega_1$')
+	plt.plot(tpoints, w2, 'g-', label = r'$\omega_2$')
+	plt.xlabel('Time (s)')
+	plt.ylabel('Time-Dependent Variable')
+	plt.legend(loc = 'best')
+	plt.show()
 
-	return (theta1_points, theta2_points, w1_points, w2_points)
+	return (w1, w2, theta1, theta2)
 
-theta1_points, theta2_points, w1_points, w2_points = LeapFrog_Int()
-theta1_points = np.array(theta1_points)
-theta2_points = np.array(theta2_points)
-w1_points = np.array(w1_points)
-w2_points = np.array(w2_points)
+w1, w2, theta1, theta2 = LeapFrog_Int()
 
-def EnergyPlotter(theta1, theta2, w1, w2):
-	"""Constants"""
-	m = 1.0
-	g = 9.8
-	l = 0.4
-	ti = 0.0
-	tf = 10.0
-	N = 1000
-	h = (tf - ti)/N
-	tpoints = np.arange(ti, tf, h)
-
+def EnergyPlotter(w1, w2, theta1, theta2):
 	"""Total Energy"""
-	Energy = m*l**2*(w1**2 + 0.5*w2**2 + w1*w2*np.cos(theta1-theta2)) - m*g*l*(2*np.cos(theta1) + np.cos(theta2))
+	Energy = m*L**2*(w1**2 + 0.5*w2**2 + w1*w2*np.cos(theta1-theta2)) - m*g*L*(2*np.cos(theta1) + np.cos(theta2))
 
 	"""Total Energy Plot as a function of Time"""
 	plt.plot(tpoints, Energy, 'k-')
 	plt.xlabel('Time (s)')
 	plt.ylabel('Energy (Joules)')
-	plt.savefig('LeapFrog_Energy.png')
+	plt.show()
 
-EnergyPlotter(theta1_points, theta2_points, w1_points, w2_points)
-animator = PendulumAnimator()
-animator.set_data((theta1_points, theta2_points))
-animator.animate()
+EnergyPlotter(w1, w2, theta1, theta2)
+# animator = PendulumAnimator()
+# animator.set_data((theta1, theta2))
+# animator.animate()
 
 
 
