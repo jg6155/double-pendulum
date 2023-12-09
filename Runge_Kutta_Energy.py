06872432315c34cd09999a7398e49e34e7cec9b8
Robert Zhao <144252970+LagrangePointL3@users.noreply.github.com>
2023-12-08 20:09:20 -0500

    Update to Runge_Kutta_Energy_2.0.py

--- Runge_Kutta_Energy.py
+++ Runge_Kutta_Energy.py
@@ -9,22 +9,23 @@ Created on Fri Nov 17 22:02:13 2023
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Initial Conditions for comparing the answeres of two integrations
-# Attention! The codes may not run unless the time interval is higher than 50 per second.
+#Initial Conditions
 L = 0.4
 g = 9.8
-m = 1
 time = 100
-interval = time*50
-initial_theta_1 = 10
-initial_theta_2 = 10
 
-# Input: length of string, g, initial theta_1, initial theta_2, time, intervals
-def position(L, g, theta_1, theta_2, t, N):
+#The time steps should be less than 0.02s
+timestep = 0.01
+
+#Input the theta degrees in radians
+initial_theta_1 = np.pi/2
+initial_theta_2 = np.pi/2
+
+def runge_kutta(L, g, theta_1, theta_2, t, N):
     a = 0.0
     b = t 
     # h is the time interval
-    h = (b-a)/N 
+    h = N
 
 
     tpoints = np.arange(a,b,h)
@@ -34,51 +35,53 @@ def position(L, g, theta_1, theta_2, t, N):
     theta2_points = []
     w1_points = []
     w2_points = []
+    
     # initial velocity
     w1 = 0
     w2 = 0
     for t in tpoints:
-        theta1_points.append(theta_1)
-        theta2_points.append(theta_2)
-        w1_points.append(w1)
-        w2_points.append(w2)
+        
         k11 = h*dp1(w1, w2 ,t, theta_1, theta_2)
         k12 = h*dp2(w1, w2 ,t, theta_1, theta_2)
+        k13 = h*dp3(w1)
+        k14 = h*dp4(w2)
+        
+        
+        k21 = h*dp1(w1 + 0.5*k11, w2 + 0.5*k12, t + 0.5*h, theta_1 + 0.5*k13, theta_2 + 0.5*k14)
+        k22 = h*dp2(w1 + 0.5*k11, w2 + 0.5*k12, t + 0.5*h, theta_1 + 0.5*k13, theta_2 + 0.5*k14 )
+        k23 = h*dp3(w1 + 0.5*k11)
+        k24 = h*dp4(w2 + 0.5*k12)
     
-        k21 = h*dp1(w1 + 0.5*k11, w2 + 0.5*k12, t + 0.5*h, theta_1, theta_2)
-        k22 = h*dp2(w1 + 0.5*k11, w2 + 0.5*k12, t + 0.5*h, theta_1, theta_2)
+        k31 = h*dp1(w1 + 0.5*k21, w2 + 0.5*k22, t + 0.5*h, theta_1 + 0.5*k23, theta_2 + 0.5*k24)
+        k32 = h*dp2(w1 + 0.5*k21, w2 + 0.5*k22, t + 0.5*h, theta_1 + 0.5*k23, theta_2 + 0.5*k24)
+        k33 = h*dp3(w1 + 0.5*k21)
+        k34 = h*dp4(w2 + 0.5*k22)
     
-        k31 = h*dp1(w1 + 0.5*k21, w2 + 0.5*k22, t + 0.5*h, theta_1, theta_2)
-        k32 = h*dp2(w1 + 0.5*k21, w2 + 0.5*k22, t + 0.5*h, theta_1, theta_2)
-    
-        k41 = h*dp1(w1 + k31, w2 + k32, t + h, theta_1, theta_2)
-        k42 = h*dp2(w1 + k31, w2 + k32, t + h, theta_1, theta_2)
-    
+        k41 = h*dp1(w1 + k31, w2 + k32, t + h, theta_1 + k33, theta_2 + k34)
+        k42 = h*dp2(w1 + k31, w2 + k32, t + h, theta_1 + k33, theta_2 + k34)
+        k43 = h*dp3(w1 + k31)
+        k44 = h*dp4(w2 + k32)
+        
+        #Update w1, w2, theta1 and theta2
         w1 += (k11 + 2*k21 + 2*k31 + k41)/6
         w2 += (k12 + 2*k22 + 2*k32 + k42)/6
+        theta_1 += (k13 + 2*k23 + 2*k33 + k43)/6
+        theta_2 += (k14 + 2*k24 + 2*k34 + k44)/6
+        
+        #Record w1, w2, theta1 and theta2
+        w1_points.append(w1)
+        w2_points.append(w2)
+        theta1_points.append(theta_1)
+        theta2_points.append(theta_2)
+        
     
-        theta_1 += w1*h
-        theta_2 += w2*h
-
-    
-    #plt.figure(figsize=(12, 4))    
-    #plt.plot(tpoints, theta1_points, label = "theta_1")
-    #plt.plot(tpoints, theta2_points, label = "theta_2")
-    #plt.xlabel("t")
-    #plt.ylabel("degree of angle theta")
-    #plt.legend()
-  
-    #txt1="The initial theta_1 is: " + str(theta_1)
-    #txt2 ="  The initial theta_2 is: " + str(theta_2)
-    #plt.figtext(0.5, 0.01, txt1 + txt2, wrap=True, horizontalalignment='center', fontsize=12)
-    #plt.show()
-    
-    return w1_points, w2_points, theta1_points, theta2_points
-
+    return theta1_points,theta2_points, w1_points, w2_points, tpoints
 
 # define the two first-order differentiation equation
 # Equation for w1
 def dp1(w1, w2, t, theta_1, theta_2):
+    g = 9.8
+    L = 0.4
     w1_term = w1**2*(np.sin(2*theta_1 - 2*theta_2))
     w2_term = 2*w2**2*np.sin(theta_1 - theta_2)
     ###
@@ -88,18 +91,33 @@ def dp1(w1, w2, t, theta_1, theta_2):
 
 # Equation for w2
 def dp2(w1, w2, t, theta_1, theta_2):
+    g = 9.8
+    L = 0.4
     w1_term = 4*w1**2*(np.sin(theta_1 - theta_2))
     w2_term = w2**2*(np.sin(2*theta_1 - 2*theta_2))
     numerator = w1_term + w2_term + (2*g/L)*(np.sin(2*theta_1 - theta_2) - np.sin(theta_2))
     denom = 3 - np.cos(2*theta_1 - 2*theta_2)
     return numerator/denom
 
+# Equation for theta1
+def dp3(w1):
+    return w1
 
+# Equation for theta2
+def dp4(w2):
+    return w2
+
+# Method used to calculate total energy
 def energy(w_1, w_2, theta_1, theta_2):
     total_en_list = []
     T = 0
     V = 0
-    for i in np.arange(0,interval):
+    m = 1
+    
+    # Number of Timesteps
+    interval = int(time/timestep)
+    
+    for i in np.arange(0, interval):
         T = m*(L**2)*(w_1[i]**2 + 0.5*w_2[i]**2 + w_1[i]*w_2[i]*np.cos(theta_1[i] - theta_2[i]))
         V = -m*g*L*(2*np.cos(theta_1[i]) + np.cos(theta_2[i]))
         total_en = T + V
@@ -108,17 +126,20 @@ def energy(w_1, w_2, theta_1, theta_2):
     time_list = np.linspace(0, time, interval)
     
     # Make the plot
-    plt.figure(figsize=(12, 4))  
-    plt.plot(time_list, total_en_list)
-    plt.xlabel("t(second)")
-    plt.ylabel("degree of angle theta(degree)")
-    plt.show()
     
+    fig = plt.figure(figsize = (7,4))
+    #plt.figure(figsize = (16,6))
+    plt.plot(time_list, total_en_list, label = r"Total Energy")
+    plt.title("Total Energy under RK method for 1000s")
+    plt.xlabel("time(s)")
+    plt.ylabel("Total Energy(J)")
+    plt.legend(loc = 'upper right')
+    txt=r"Under Condition of part (b): initial $\theta_1 = \theta_2= \pi/2$, initial $\omega_1 = \theta_2 = 0$, L = 0.4m, $g = 9.8m~s^{-2}$, m = 1kg "
+    fig.text(.5, .0001, txt, ha='center')
+    plt.tight_layout()
+    plt.show()
+
     return total_en_list
 
-
-w1, w2, theta1, theta2 = position(L, g, initial_theta_1*np.pi/180, initial_theta_2*np.pi/180, time, interval)
+theta1, theta2, w1, w2,tpoints = runge_kutta(L, g, initial_theta_1, initial_theta_2, time, timestep)
 total_energy = energy(w1, w2, theta1, theta2)
-
-
-
