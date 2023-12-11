19294acae20e5964e045d93e975444596e0b8acb
Jorge Gomez <jg6155@nyu.edu>
2023-12-10 20:24:42 -0500

    poincare

---
+++ poincaré.py
@@ -0,0 +1,117 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from runge_kutta import runge_kutta
+
+class Poincare:
+    def __init__(self, L, g, time, interval, num_nearby_points, epsilon):
+        self.L = L
+        self.g = g
+        self.time = time
+        self.interval = interval
+        self.num_nearby_points = num_nearby_points
+        self.epsilon = epsilon
+
+    def generate_nearby_points(self, fiducial):
+        """Generate nearby points around the fiducial point."""
+        nearby_points = []
+        for _ in range(self.num_nearby_points):
+            perturbation = np.random.normal(0, self.epsilon, 4)
+            point = np.array(fiducial) + perturbation
+            nearby_points.append(point)
+        return nearby_points
+
+    def euclidean_distance(self, p1, p2):
+        """Calculate Euclidean distance between two points in 4D space."""
+        return np.linalg.norm(np.array(p1) - np.array(p2))
+
+    def run_simulation(self, initial_conditions):
+        """Run the simulation for given initial conditions."""
+        # Ensure initial_conditions is a tuple of length 4 (theta1, theta2, w1, w2)
+        if len(initial_conditions) != 4:
+            raise ValueError("Initial conditions should be a tuple of four values: theta1, theta2, w1, w2")
+
+        initial_theta_1, initial_theta_2, initial_w1, initial_w2 = initial_conditions
+        # Call the runge_kutta function with the correct number of arguments
+        return runge_kutta(self.L, self.g, initial_theta_1, initial_theta_2, self.time, self.interval)
+    
+    def generate_poincare_section(self, initial_conditions):
+        """Generate and plot a Poincaré section for given initial conditions."""
+        theta1_vals, theta2_vals, w1_vals, w2_vals = self.run_simulation(initial_conditions)
+
+        poincare_theta2 = []
+        poincare_w2 = []
+
+        # Map theta values to range [-π, π]
+        theta1_vals = np.arctan2(np.sin(theta1_vals), np.cos(theta1_vals))
+        theta2_vals = np.arctan2(np.sin(theta2_vals), np.cos(theta2_vals))
+        # Define the Poincaré section conditions (θ1 = 0 and θ1_dot > 0)
+        for i in range(1, len(theta1_vals)):
+            if abs(theta1_vals[i]) < .1 and w1_vals[i] > 0:
+                poincare_theta2.append(theta2_vals[i])
+                poincare_w2.append(w2_vals[i])
+
+        plt.figure(figsize=(8, 6))
+        plt.scatter(poincare_theta2, poincare_w2, s=10)
+        plt.xlabel('Theta2')
+        plt.ylabel('Angular Velocity w2')
+        plt.title(f'Poincaré Section for Initial Conditions: {initial_conditions}')
+        plt.show()
+
+    
+    def analyze_trajectories(self, fiducial_points):
+        for idx, fiducial in enumerate(fiducial_points):
+            nearby_points = self.generate_nearby_points(fiducial)
+            distances_over_time = []
+
+            # Simulate fiducial trajectory
+            theta1_vals_fiducial, theta2_vals_fiducial, w1_vals_fiducial, w2_vals_fiducial = self.run_simulation(fiducial)
+
+            # Simulate nearby trajectories and calculate distances
+            for nearby in nearby_points:
+                theta1_vals, theta2_vals, w1_vals, w2_vals = self.run_simulation(nearby)
+                distances = [self.euclidean_distance((t1, t2, w1, w2), (t1f, t2f, w1f, w2f)) 
+                             for t1, t2, w1, w2, t1f, t2f, w1f, w2f in zip(theta1_vals, theta2_vals, w1_vals, w2_vals, theta1_vals_fiducial, theta2_vals_fiducial, w1_vals_fiducial, w2_vals_fiducial)]
+                distances_over_time.append(distances)
+
+            # Calculate cumulative differences and select trajectories
+            cumulative_differences = [np.sum(distances) for distances in distances_over_time]
+            max_diff_idx = np.argmax(cumulative_differences)
+            min_diff_idx = np.argmin(cumulative_differences)
+            median_diff_idx = np.argsort(cumulative_differences)[len(cumulative_differences) // 2]
+
+            # Create a new figure for each fiducial point
+            plt.figure(figsize=(10, 5))
+            for selected_idx in [max_diff_idx, median_diff_idx, min_diff_idx]:
+                initial_conditions = nearby_points[selected_idx]
+                label = f'IC: {", ".join(f"{val:.2f}" for val in initial_conditions)}'
+                plt.plot(range(len(distances_over_time[selected_idx])), distances_over_time[selected_idx], label=label)
+
+            # Set titles and labels for the plot
+            fiducial_str = ', '.join(f"{val:.4g}" for val in fiducial) if isinstance(fiducial, tuple) else f"{fiducial:.4g}"
+            plt.title(f'Distances for Energy Starting at ({fiducial_str})')
+            plt.xlabel('Time Steps')
+            plt.ylabel('Distance from Fiducial Trajectory')
+            plt.legend()
+
+            # Display the plot
+            plt.show()
+
+# Example usage
+poincare = Poincare(L=0.4, g=9.8, time=100, interval=5000, num_nearby_points=10, epsilon=0.1)
+epsilon = .1
+# stationary_points = [
+#     (0, 0, 0, 0),
+#     (np.pi , 0, 0, 0),
+#     (np.pi , np.pi, 0, 0)
+# ]
+# poincare.analyze_trajectories(stationary_points)
+
+initial_conditions_sets = [
+    (0 + epsilon, 0, 0, 0),  # Both pendulums slightly perturbed from hanging down
+    (np.pi + epsilon, 0, 0, 0),  # One pendulum inverted, the other hanging down
+    (np.pi + epsilon, np.pi + epsilon, 0, 0)  # Both pendulums slightly perturbed from being inverted
+]
+
+# Generate Poincaré sections for each set of initial conditions
+for ic in initial_conditions_sets:
+    poincare.generate_poincare_section(ic)
--- runge_kutta.py
+++ runge_kutta.py
@@ -10,65 +10,58 @@ Created on Fri Oct 27 19:33:14 2023
 
 import numpy as np
 
-def runge_kutta(L, g, theta_1, theta_2, t, N):
+def runge_kutta(L, g, initial_theta_1, initial_theta_2, time, interval):
+    def dp1(w1, w2, theta_1, theta_2):
+        # Differential equation for w1
+        w1_term = w1**2 * np.sin(2*theta_1 - 2*theta_2)
+        w2_term = 2*w2**2 * np.sin(theta_1 - theta_2)
+        numerator = -w1_term - w2_term - (g/L) * (np.sin(theta_1 - 2*theta_2) + 3*np.sin(theta_1))
+        denom = 3 - np.cos(2*theta_1 - 2*theta_2)
+        return numerator/denom
+
+    def dp2(w1, w2, theta_1, theta_2):
+        # Differential equation for w2
+        w1_term = 4*w1**2 * np.sin(theta_1 - theta_2)
+        w2_term = w2**2 * np.sin(2*theta_1 - 2*theta_2)
+        numerator = w1_term + w2_term + (2*g/L) * (np.sin(2*theta_1 - theta_2) - np.sin(theta_2))
+        denom = 3 - np.cos(2*theta_1 - 2*theta_2)
+        return numerator/denom
+
     a = 0.0
-    b = t 
-    # h is the time interval
-    h = (b-a)/N 
+    b = time
+    h = (b - a) / interval
 
+    tpoints = np.arange(a, b, h)
 
-    tpoints = np.arange(a,b,h)
-    
-    # Create two lists to store the angle degrees
-    theta1_points = []
-    theta2_points = []
-    w1_points = []
-    w2_points = []
-    
-    # initial velocity
-    w1 = 0
-    w2 = 0
+    theta1 = initial_theta_1
+    theta2 = initial_theta_2
+    w1 = 0.0
+    w2 = 0.0
+
+    theta1_points, theta2_points, w1_points, w2_points = [], [], [], []
+
     for t in tpoints:
-        theta1_points.append(theta_1)
-        theta2_points.append(theta_2)
-        k11 = h*dp1(w1, w2 ,t, theta_1, theta_2)
-        k12 = h*dp2(w1, w2 ,t, theta_1, theta_2)
-    
-        k21 = h*dp1(w1 + 0.5*k11, w2 + 0.5*k12, t + 0.5*h, theta_1, theta_2)
-        k22 = h*dp2(w1 + 0.5*k11, w2 + 0.5*k12, t + 0.5*h, theta_1, theta_2)
-    
-        k31 = h*dp1(w1 + 0.5*k21, w2 + 0.5*k22, t + 0.5*h, theta_1, theta_2)
-        k32 = h*dp2(w1 + 0.5*k21, w2 + 0.5*k22, t + 0.5*h, theta_1, theta_2)
-    
-        k41 = h*dp1(w1 + k31, w2 + k32, t + h, theta_1, theta_2)
-        k42 = h*dp2(w1 + k31, w2 + k32, t + h, theta_1, theta_2)
-    
-        w1 += (k11 + 2*k21 + 2*k31 + k41)/6
-        w2 += (k12 + 2*k22 + 2*k32 + k42)/6
+        theta1_points.append(theta1)
+        theta2_points.append(theta2)
+        w1_points.append(w1)
+        w2_points.append(w2)
 
-        #put it here because it's the change in theta1, theta2
-        w2_points.append(w1)
-        w1_points.append(w2)
-    
-        theta_1 += w1*h
-        theta_2 += w2*h
-    
-    return theta1_points,theta2_points, w1_points, w2_points
+        k11 = h * dp1(w1, w2, theta1, theta2)
+        k12 = h * dp2(w1, w2, theta1, theta2)
 
-# define the two first-order differentiation equation
-# Equation for w1
-def dp1(w1, w2, t, theta_1, theta_2):
-    w1_term = w1**2*(np.sin(2*theta_1 - 2*theta_2))
-    w2_term = 2*w2**2*np.sin(theta_1 - theta_2)
-    ###
-    numerator = w1_term*(-1) - w2_term - (g/L)*(np.sin(theta_1 - 2*theta_2) + 3*np.sin(theta_1))
-    denom = 3 - np.cos(2*theta_1 - 2*theta_2)
-    return numerator/denom
+        k21 = h * dp1(w1 + 0.5*k11, w2 + 0.5*k12, theta1, theta2)
+        k22 = h * dp2(w1 + 0.5*k11, w2 + 0.5*k12, theta1, theta2)
 
-# Equation for w2
-def dp2(w1, w2, t, theta_1, theta_2):
-    w1_term = 4*w1**2*(np.sin(theta_1 - theta_2))
-    w2_term = w2**2*(np.sin(2*theta_1 - 2*theta_2))
-    numerator = w1_term + w2_term + (2*g/L)*(np.sin(2*theta_1 - theta_2) - np.sin(theta_2))
-    denom = 3 - np.cos(2*theta_1 - 2*theta_2)
-    return numerator/denom
+        k31 = h * dp1(w1 + 0.5*k21, w2 + 0.5*k22, theta1, theta2)
+        k32 = h * dp2(w1 + 0.5*k21, w2 + 0.5*k22, theta1, theta2)
+
+        k41 = h * dp1(w1 + k31, w2 + k32, theta1, theta2)
+        k42 = h * dp2(w1 + k31, w2 + k32, theta1, theta2)
+
+        w1 += (k11 + 2*k21 + 2*k31 + k41) / 6
+        w2 += (k12 + 2*k22 + 2*k32 + k42) / 6
+
+        theta1 += w1 * h
+        theta2 += w2 * h
+
+    return theta1_points, theta2_points, w1_points, w2_points
