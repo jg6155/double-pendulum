7b5ca5e2376a6334693f27d9555aaa94c81a5770
Robert Zhao <144252970+LagrangePointL3@users.noreply.github.com>
2023-12-11 15:51:04 -0500

    Update Runge_Kutta_test_integrators.py

--- Runge_Kutta_test_integrators.py
+++ Runge_Kutta_test_integrators.py
@@ -9,7 +9,7 @@ Created on Fri Nov 17 21:15:28 2023
 import numpy as np
 import matplotlib.pyplot as plt
 from animator import PendulumAnimator
-from DoublePendulum_Funcs import w1_dot, w2_dot, L, g, m
+from DoublePendulum_Funcs import w1_dot, w2_dot, g, m
 from Runge_Kutta_method import runge_kutta
 
 #Initial Conditions
